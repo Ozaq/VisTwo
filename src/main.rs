37bd75cc265bfd303f7f88d4400bfb10c1d6d85d
Ozaq <ozaq@posteo.de>
2021-12-24 21:35:28 +0100

    add simple triangle

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ use glium::glutin::event::{Event, WindowEvent};
 use glium::glutin::event_loop::{ControlFlow, EventLoop};
 use glium::glutin::window::WindowBuilder;
 use glium::glutin::ContextBuilder;
-use glium::{Display, Surface};
+use glium::{Display, Frame, Surface};
 use imgui::{Condition, Context, MenuItem, Ui, Window};
 use imgui_glium_renderer::Renderer;
 use imgui_winit_support::{HiDpiMode, WinitPlatform};
 use winit::window::Fullscreen;
+
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 2],
+}
+glium::implement_vertex!(Vertex, position);
 
 pub struct System {
     pub display: Display,
@@ -54,9 +60,10 @@ impl System {
         }
     }
 
-    pub fn enter_main_loop<F>(self, mut draw_ui: F)
+    pub fn enter_main_loop<Fn1, Fn2>(self, mut draw_ui: Fn1, draw_content: Fn2)
     where
-        F: FnMut(&mut bool, &mut Ui) + 'static,
+        Fn1: FnMut(&mut bool, &mut Ui) + 'static,
+        Fn2: Fn(&mut Frame) + 'static,
     {
         let Self {
             display,
@@ -95,6 +102,7 @@ impl System {
                 renderer
                     .render(&mut target, draw_data)
                     .expect("Rendering failed!");
+                draw_content(&mut target);
                 target.finish().expect("Falied to swap buffers!");
             }
             Event::WindowEvent {
@@ -110,23 +118,75 @@ impl System {
 
 fn main() {
     let system = System::new();
-    system.enter_main_loop(move |_, ui| {
-        let io = ui.io();
-        Window::new("Hello!")
-            .size(io.display_size, Condition::Always)
-            .position([0f32, 0f32], Condition::Always)
-            .no_decoration()
-            .menu_bar(true)
-            .movable(false)
-            .build(ui, || {
-                ui.menu_bar(|| {
-                    let file_clicked = MenuItem::new("File").build(ui);
-                    if file_clicked {
-                        println!("FILE CLICKED!!");
-                    }
-                    MenuItem::new("About").build(ui);
-                    MenuItem::new("Exit").build(ui);
-                })
-            });
-    });
+    let shape = vec![
+        Vertex {
+            position: [-0.5, -0.5],
+        },
+        Vertex {
+            position: [0.0, 0.5],
+        },
+        Vertex {
+            position: [0.5, -0.25],
+        },
+    ];
+    let vertex_buffer = glium::VertexBuffer::new(&system.display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+    let program = glium::Program::from_source(
+        &system.display,
+        vertex_shader_src,
+        fragment_shader_src,
+        None,
+    )
+    .unwrap();
+
+    system.enter_main_loop(
+        move |_, ui| {
+            let io = ui.io();
+            Window::new("Hello!")
+                .size(io.display_size, Condition::Always)
+                .position([0f32, 0f32], Condition::Always)
+                .no_decoration()
+                .menu_bar(true)
+                .movable(false)
+                .build(ui, || {
+                    ui.menu_bar(|| {
+                        let file_clicked = MenuItem::new("File").build(ui);
+                        if file_clicked {
+                            println!("FILE CLICKED!!");
+                        }
+                        MenuItem::new("About").build(ui);
+                        MenuItem::new("Exit").build(ui);
+                    })
+                });
+        },
+        move |target| {
+            target
+                .draw(
+                    &vertex_buffer,
+                    &indices,
+                    &program,
+                    &glium::uniforms::EmptyUniforms,
+                    &Default::default(),
+                )
+                .unwrap();
+        },
+    );
 }
